31a486a2e028c2907b7e9c08d3de74c894e552f9
Yannic Hock <yaho00002@htwsaar.de>
2026-03-29 18:46:31 +0200

    Add contact form component with styling and submission handling

--- src/components/FloralBackground.tsx
+++ src/components/FloralBackground.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 export const FloralBackground: React.FC = () => {
+  const baseUrl = import.meta.env.BASE_URL;
+
   return (
     <div className="floral-background" aria-hidden="true">
-      <img src="/svg/hanging.svg" className="floral-svg hanging-1" alt="" />
-      <img src="/svg/hanging%202.svg" className="floral-svg hanging-2" alt="" />
-      <img src="/svg/Farn%201.svg" className="floral-svg farn-1" alt="" />
-      <img src="/svg/Farn%202.svg" className="floral-svg farn-2" alt="" />
-      <img src="/svg/Fineline.svg" className="floral-svg fineline-1" alt="" />
+      <img src={`${baseUrl}svg/hanging.svg`} className="floral-svg hanging-1" alt="" />
+      <img src={`${baseUrl}svg/hanging%202.svg`} className="floral-svg hanging-2" alt="" />
+      <img src={`${baseUrl}svg/Farn%201.svg`} className="floral-svg farn-1" alt="" />
+      <img src={`${baseUrl}svg/Farn%202.svg`} className="floral-svg farn-2" alt="" />
+      <img src={`${baseUrl}svg/Fineline.svg`} className="floral-svg fineline-1" alt="" />
     </div>
   );
 };
